5102fbc3f330194a16eb4b140544812cbddb1772
Alexsander-Trybe <Alexsander81002745@gmail.com>
2022-04-29 12:50:18 -0300

    Classe Rentals - Cria atributos privados: String name, Cars car, Customers customer, Date (startDate, endDate, createdAt, updatedAt), int total

--- java-aluga-carros/src/model/Rentals.java
+++ java-aluga-carros/src/model/Rentals.java
@@ -1,5 +1,12 @@
 package model;
 
-public class Rentals {
+import java.sql.Date;
 
+public class Rentals {
+  private String name;
+  private Cars car;
+  private Customers customer;
+  private Date startDate, endDate;
+  private Date createdAt, updatedAt;
+  private int total;
 }
