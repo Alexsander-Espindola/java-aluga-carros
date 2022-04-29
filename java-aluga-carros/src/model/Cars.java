5143ffc95eb5f2a693f44dbc282e8c130169f596
Alexsander-Trybe <Alexsander81002745@gmail.com>
2022-04-29 12:26:39 -0300

    Classe Cars - Cria atributos privados: String name, String description, short dailyRate, short avaliabe, String licensePlate, Brand brand, Categories category, String color, Date createdAt

--- java-aluga-carros/src/model/Cars.java
+++ java-aluga-carros/src/model/Cars.java
@@ -1,5 +1,15 @@
 package model;
 
-public class Cars {
+import java.sql.Date;
 
+public class Cars {
+  private String name;
+  private String description;
+  private short dailyRate;
+  private short avaliable;
+  private String licensePlate;
+  private Brand brand;
+  private Categories category;
+  private String color;
+  private Date createdAt;
 }
