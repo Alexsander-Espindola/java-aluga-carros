3bcc1be71a11f31b77aad50c776384ef7e2805aa
Alexsander-Trybe <Alexsander81002745@gmail.com>
2022-04-30 20:48:25 -0300

    Refatora CustomerController - Cria verifyBirthDate e após a verificação de todos os dados instanciar um objeto Customers e chama customers.addCustomerInDB para futuramente passar o objeto para o bando de dados

--- java-aluga-carros/src/aluga/main.java
+++ java-aluga-carros/src/aluga/main.java
@@ -1,16 +1,11 @@
 package aluga;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class main {
 
   public static void main(String[] args) {
     // TODO Auto-generated method stub
-    String regexPhoneNumber = "^((\\(\\d{2}\\))|\\d{5})[- .]?\\d{5}[- .]?\\d{4}$";
-    Pattern pattern = Pattern.compile(regexPhoneNumber);
-    Matcher matcher = pattern.matcher("(62) 99360-5888");
-    System.out.println(matcher.matches());
+    String regexDate = "02/05/2000";
+    System.out.println(regexDate.matches("\\d{2}/\\d{2}/\\d{4}"));
   }
 
 }
--- java-aluga-carros/src/controller/CustomerController.java
+++ java-aluga-carros/src/controller/CustomerController.java
@@ -1,22 +1,32 @@
 package controller;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import model.Customers;
 
 
 
 public class CustomerController {
   
-  public boolean registerCustomer(String name, LocalDate birthDate, String email, String driverLicense, String adress, String phoneNumber) {
+  public boolean registerCustomer(String name, String birthDate, String email, String driverLicense, String adress, String phoneNumber) {
     boolean bolName = name != null && name.length() > 0;
-    boolean bolBirtDate = birthDate != null;
+    boolean bolBirtDate = verifyBirthDate(birthDate);
     boolean bolEmail = verifyEmail(email);
     boolean bolDriverLicense = driverLicense != null && driverLicense.length() > 0;
     boolean bolAdress = adress != null && adress.length() > 0;
     boolean bolPhoneNumber = verifyPhoneNumber(phoneNumber);
     
-    if (bolName && bolBirtDate && bolEmail && bolDriverLicense && bolAdress && bolPhoneNumber) return true;
+    if (bolName && bolBirtDate && bolEmail && bolDriverLicense && bolAdress && bolPhoneNumber) {
+      DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy"); 
+      LocalDate dateBirth = LocalDate.parse(birthDate, formato);
+      Customers customer = new Customers(name, dateBirth, email, driverLicense, adress, phoneNumber);
+      customer.addCustomerInDB(customer);
+
+      return true;
+    }
     
     return false;  
   }
@@ -36,6 +46,12 @@ public class CustomerController {
     Pattern pattern = Pattern.compile(regexPhoneNumber);
     Matcher matcherPhoneNumber = pattern.matcher(phoneNumber);
     if (matcherPhoneNumber.matches()) return true;
+    
+    return false;
+  }
+  
+  public boolean verifyBirthDate(String birthDate) {
+    if (birthDate.matches("\\\\d{2}/\\\\d{2}/\\\\d{4}")) return true;
     
     return false;
   }
