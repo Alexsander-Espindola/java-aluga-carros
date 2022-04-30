14156455ed4778b8ffd22579f3aef35ceaed9afb
Alexsander-Trybe <Alexsander81002745@gmail.com>
2022-04-30 17:43:27 -0300

    Classe Brand: Cria getters e setters. Anotação: a classe Brand tem um array list da classe carro e uma função addCar para adicionar o carro a marca, mas isso pode mudar quando eu começar a guardar as informações no banco de dado. Eu usarei essa lógica nas classes seguintes (Categories, Rentals e Customers)

--- java-aluga-carros/src/aluga/main.java
+++ java-aluga-carros/src/aluga/main.java
@@ -4,9 +4,7 @@ public class main {
 
   public static void main(String[] args) {
     // TODO Auto-generated method stub
-    String name = "";
-    boolean bolName = name != null && name.length() < 0;
-    System.out.println(bolName);
+
   }
 
 }
