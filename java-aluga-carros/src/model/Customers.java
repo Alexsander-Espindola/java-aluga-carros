c18d19c98253fee062802d17920182e2906bb388
Alexsander-Trybe <Alexsander81002745@gmail.com>
2022-04-30 18:59:11 -0300

    Cria CustomerController. Refatora model Customers - Cria getters, setters e addCustomerInDB

--- java-aluga-carros/src/model/Customers.java
+++ java-aluga-carros/src/model/Customers.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.sql.Date;
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Customers {
@@ -10,7 +11,92 @@ public class Customers {
   private String driverLicense;
   private String adress;
   private int phoneNumber;
-  private Date createdAt;
-  private Date updatedAt;
+  private LocalDate createdAt;
+  private LocalDate updatedAt;
   private ArrayList<Rentals> rentalHistory = new ArrayList<Rentals>();
+ 
+  public Customers(String name, Date birthDate, String email, String driverLicense, String adress, int phoneNumber) {
+    this.name = name;
+    this.birthDate = birthDate;
+    this.email = email;
+    this.driverLicense = driverLicense;
+    this.adress = adress;
+    this.phoneNumber = phoneNumber;
+    this.createdAt = LocalDate.now();
+    this.updatedAt = LocalDate.now();
+  }
+  
+  public void addCustomerInDB(Customers customer) {
+    
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Date getBirthDate() {
+    return birthDate;
+  }
+
+  public void setBirthDate(Date birthDate) {
+    this.birthDate = birthDate;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getDriverLicense() {
+    return driverLicense;
+  }
+
+  public void setDriverLicense(String driverLicense) {
+    this.driverLicense = driverLicense;
+  }
+
+  public String getAdress() {
+    return adress;
+  }
+
+  public void setAdress(String adress) {
+    this.adress = adress;
+  }
+
+  public int getPhoneNumber() {
+    return phoneNumber;
+  }
+
+  public void setPhoneNumber(int phoneNumber) {
+    this.phoneNumber = phoneNumber;
+  }
+
+  public LocalDate getUpdatedAt() {
+    return updatedAt;
+  }
+
+  public void setUpdatedAt() {
+    this.updatedAt = LocalDate.now();
+  }
+
+  public ArrayList<Rentals> getRentalHistory() {
+    return rentalHistory;
+  }
+
+  public void setRentalHistory(Rentals rentalHistory) {
+    this.rentalHistory.add(rentalHistory);
+  }
+
+  public LocalDate getCreatedAt() {
+    return createdAt;
+  }
+  
+  
 }
