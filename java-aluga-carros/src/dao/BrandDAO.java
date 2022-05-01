917aea9510f3329d2852d2e7ff854a2670224692
Alexsander-Trybe <Alexsander81002745@gmail.com>
2022-05-01 18:40:05 -0300

    Conecta Brand ao banco de dados e passa as informações da tela de cadastro da Brand para sua tabela

--- java-aluga-carros/src/dao/BrandDAO.java
+++ java-aluga-carros/src/dao/BrandDAO.java
@@ -1,5 +1,46 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import model.Brand;
+
 public class BrandDAO {
 
+  public void addBrandInDB(Brand brand) throws ExceptionDAO {
+    String sql = "insert into BRAND (name, createdAt) value (?, ?)";
+    PreparedStatement pStatement = null;
+    Connection conn = null;
+
+    try {
+      conn = new ConnectionSQL().getConnection();
+      pStatement = conn.prepareStatement(sql);
+      pStatement.setString(1, brand.getName());
+      pStatement.setString(2, brand.getCreatedAt());
+      pStatement.execute();
+    } catch (SQLException error) {
+      // TODO: handle exception
+      throw new ExceptionDAO("Erro ao cadastrar o carro: " + error.getMessage());
+    } finally {
+
+      try {
+        if (pStatement != null) {
+          pStatement.close();
+        }
+      } catch (SQLException error) {
+        // TODO: handle exception
+        throw new ExceptionDAO("Erro ao fechar o statement: " + error.getMessage());
+      }
+
+      try {
+        if (conn != null) {
+          conn.close();
+        }
+      } catch (SQLException error) {
+        // TODO: handle exception
+        throw new ExceptionDAO("Erro ao fechar a conexão: " + error.getMessage());
+      }
+    }
+  }
 }
